92258c1bf8d580c8c2c08a98299277d4a2415856
Matt Seidholz <mseidholz@gmail.com>
2020-10-30 10:29:51 -0400

    removing coverage & linting from circle-ci config (for now)

--- src/Containers/SideBar/SideBar.js
+++ src/Containers/SideBar/SideBar.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./SideBar.css";
 import Nav from "react-bootstrap/Nav";
-import {v4 as uuidv4} from 'uuid'
+import { v4 as uuidv4 } from "uuid";
 
 function SideBar({ children }) {
-
   return (
     <Nav defaultActiveKey="/home" className="sidebar flex-column">
-      {children.map(c=> <div key={uuidv4()} className="navlink-span">{c}</div>)}
+      {children.map((c) => (
+        <div key={uuidv4()} className="navlink-span">
+          {c}
+        </div>
+      ))}
     </Nav>
   );
 }
