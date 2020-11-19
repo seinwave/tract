fcbd828ba7d9b1966af47937d4e33e6a80653248
Matt Seidholz <mseidholz@gmail.com>
2020-11-19 11:32:24 -0500

    fixed failing layout tests

--- src/Containers/Layout/PageLayout.react.test.js
+++ src/Containers/Layout/PageLayout.react.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Layout from './Layout'
+import PageLayout from './PageLayout'
 import renderer from 'react-test-renderer'
 
 it('renders as expected', () => {
     const tree = renderer
-    .create(<Layout/>)
+    .create(<PageLayout/>)
     .toJSON();
     expect(tree).toMatchSnapshot(); 
 })
